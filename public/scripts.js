bd8f3cb47e54241069565d2079a75e03609500ac
Nand Kishor <nandkk07@gmail.com>
2021-03-07 22:49:28 +0530

    finished

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
+            index: 0,
+            selectedAnswer: '',
+            correctAnswer: 0,
+            wrongAnswer: 0,
+            count: 3,
             questions: [
+                
                 {
                     question: "How many moons does Mars have?",
                     answers: {a: '1', b: '2', c:'4', d:'5'},
@@ -13,11 +19,33 @@ const app = Vue.createApp({
                     correctAnswer: 'b'
                 },
                 {
-                    question: "Hey you",
+                    question: "Last Question",
                     answers: {a: 'Bags', b: 'Cat', c:'Dog', d:'Gef'},
-                    correctAnswer: 'b'
+                    correctAnswer: 'd'
                 }
             ]
+        }
+    },
+    methods: {
+        answered(e){
+            this.selectedAnswer = e.target.value
+            if (this.selectedAnswer == this.questions[this.index]['correctAnswer'])
+                this.correctAnswer++
+            else
+                this.wrongAnswer++
+        },
+        nextQuestion(){
+            this.index++
+            this.selectedAnswer = ''
+        },
+        showResults(){
+            this.index++
+        },
+        resetQuiz(){
+            this.index = 0
+            this.selectedAnswer = ''
+            this.correctAnswer = 0
+            this.wrongAnswer = 0
         }
     }
 })
